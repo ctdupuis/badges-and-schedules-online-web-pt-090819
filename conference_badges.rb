61aa9f4cc30acd4418565912993519f54594b5aa
Cody Dupuis <ctdupuis7@gmail.com>
2019-09-10 02:05:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  new = [] 
+  array.each do |element|
+    new << "Hello, my name is #{element}."
+  end
+  new
+end
+
+def assign_rooms(array)
+  rooms = []
+  array.each_with_index do |element, index|
+    rooms << "Hello, #{element}! You'll be assigned to room #{index+1}!"
+    # binding.pry
+  end
+  rooms
+end
+
+def printer(name)
+  batch_badge_creator.each do |person|
+    binding.pry
+  end
+end
